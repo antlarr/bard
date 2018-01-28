adebfde52599116be9f5c6ef085d3d722d57a7f8
Antonio Larrosa <larrosa@kde.org>
2018-01-28 12:30:58 +0100

    Add information to __init__

--- bard/__init__.py
+++ bard/__init__.py
@@ -5,3 +5,10 @@ from .bard import Bard
 
 __all__ = (Bard)
 __name__ = 'bard'
+__license__ = 'GPLv3'
+__version__ = '0.1'
+__date__ = '20180128'
+__author__ = 'Antonio Larrosa'
+__maintainer__ = 'Antonio Larrosa'
+__email__ = 'larrosa@kde.org'
+__status__ = 'Production'
