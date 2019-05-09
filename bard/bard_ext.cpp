752fbed74d19879797e18a563844e1c0384147fa
Antonio Larrosa <antonio.larrosa@gmail.com>
2019-05-09 02:02:22 +0200

    Fix copyright header to be the same as in bard_audiofile

--- bard/bard_ext.cpp
+++ bard/bard_ext.cpp
@@ -1,9 +1,10 @@
 /*
-    This file is part of Bard.
+    This file is part of Bard (https://github.com/antlarr/bard)
+    Copyright (C) 2017-2019 Antonio Larrosa <antonio.larrosa@gmail.com>
 
     Bard is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License.
+    the Free Software Foundation, version 3.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
