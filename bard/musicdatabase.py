antlarr/bard

commit 54a41648acb1d5f5990ae1f9084e98177cadfc3c

Add the concept of Album
An Album is a MusicBrainz release instance. A single directory (optionally with
subdirectories) that contains files all belonging to an unique release.
There can be more than one album with the same release, but only one
release per album.

The table album_songs contains the relation between an album and its
songs, while the materialized views album_properties and album_releases
are automatically regenerated with the audio properties of each album
and the release associated to the album, respectively.

Note that an album may have more than one audio format within.

bard/musicdatabase.py
@@ -3,6 +3,7 @@
 from bard.config import config
 from bard.normalizetags import normalizeTagValues
 from bard.utils import DecodedAudioPropertiesTuple, DecodeMessageRecord
+from bard.album import albumPath
 import sqlalchemy
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
@@ -759,6 +760,8 @@ CREATE TABLE similarities(
                     c.rollback()
                     print(sql, tags)
                     raise
+            albumID = MusicDatabase.getAlbumID(albumPath(song.path()))
+            MusicDatabase.setSongInAlbum(song.id, albumID)
 
     @staticmethod
     def removeSong(song=None, byID=None):
@@ -780,6 +783,7 @@ CREATE TABLE similarities(
         c.execute('DELETE FROM tags where song_id=:id', params)
         c.execute('DELETE FROM properties where song_id=:id', params)
         c.execute('DELETE FROM ratings where song_id=:id', params)
+        c.execute('DELETE FROM album_songs where song_id=:id', params)
         c.execute('DELETE FROM songs where id=:id', params)
         MusicDatabase.commit()
 
@@ -1494,6 +1498,77 @@ or name {like} '%%MusicBrainz/Track Id'""")
             print(f'insert {table.name} db data for {recorddict}')
             i = table.insert().values(**recorddict)
             MusicDatabase.execute(i)
+
+    @staticmethod
+    def songsWithReleaseID(onlyWithoutAlbums=True):
+        c = MusicDatabase.getCursor()
+        if onlyWithoutAlbums:
+            extra = '   AND song_id NOT IN (select song_id from album_songs)'
+        else:
+            extra = ''
+        sql = text('SELECT id, path '
+                   '  FROM songs, songs_mb '
+                   ' WHERE id = song_id '
+                   '   AND releaseid IS NOT NULL '
+                   f'   {extra}'
+                   'ORDER BY id')
+        result = c.execute(sql)
+        return [(x[0], x[1]) for x in result.fetchall()]
+
+    @staticmethod
+    def getAlbumID(albumPath):
+        c = MusicDatabase.getCursor()
+        sql = text('SELECT id FROM albums where path = :path')
+
+        result = c.execute(sql, {'path': albumPath}).fetchone()
+        if result:
+            return result[0]
+        else:
+            sql = text('INSERT INTO albums(path) VALUES (:path)')
+            c.execute(sql, {'path': albumPath})
+
+            try:
+                sql = "SELECT currval(pg_get_serial_sequence('albums','id'))"
+                result = c.execute(sql)
+            except sqlalchemy.exc.OperationalError:
+                # Try the sqlite way
+                sql = 'SELECT last_insert_rowid()'
+                result = c.execute(sql)
+
+            MusicDatabase.commit()
+            albumID = result.fetchone()[0]
+            return albumID
+
+    @staticmethod
+    def setSongInAlbum(songID, albumID):
+        record = {'song_id': songID, 'album_id': albumID}
+        album_songs = MusicDatabase.table('album_songs')
+        MusicDatabase.insert_or_update(album_songs, record,
+                                       album_songs.c.song_id == songID)
+
+    @staticmethod
+    def removeOrphanAlbums():
+        c = MusicDatabase.getCursor()
+        sql = text('DELETE FROM albums '
+                   ' WHERE id NOT IN (SELECT DISTINCT album_id '
+                   '                    FROM album_songs)')
+        result = c.execute(sql)
+        MusicDatabase.commit()
+
+        if result.rowcount:
+            print(f'{result.rowcount} orphan albums removed')
+
+    @staticmethod
+    def refreshMaterializedView(viewName):
+        c = MusicDatabase.getCursor()
+        sql = text(f'refresh materialized view {viewName}')
+        c.execute(sql)
+        MusicDatabase.commit()
+
+    @staticmethod
+    def refreshMaterializedViews():
+        MusicDatabase.refreshMaterializedView('album_properties')
+        MusicDatabase.refreshMaterializedView('album_release')
 
 
 table = MusicDatabase.table

bard/album.py
@@ -1,0 +1,17 @@
+from os.path import basename, dirname
+import re
+
+
+patterns = [re.compile(f'{medium}[0-9]*')
+            for medium in ('CD', 'LP', 'DVD', 'BD', 'SACD')]
+
+
+def isMediumPath(path):
+    return any(pat.match(path) for pat in patterns)
+
+
+def albumPath(songPath):
+    directory = dirname(songPath)
+    if isMediumPath(basename(directory)):
+        return dirname(directory)
+    return directory

bard/bard.py
@@ -31,6 +31,7 @@ from argparse import ArgumentParser
 from bard.config import config, translatePath
 from bard.user import requestNewPassword
 from bard.musicbrainz_database import MusicBrainzDatabase
+from bard.album import albumPath
 
 
 ComparisonResult = namedtuple('ComparisonResult', ['offset', 'similarity'])
@@ -326,9 +327,12 @@ class Bard:
                 print('Removing song', song.path())
                 self.db.removeSong(song)
 
+        MusicDatabase.removeOrphanAlbums()
+
         print('update musicbrainz ids')
         # do things for new songs
         MusicBrainzDatabase.updateMusicBrainzIDs(ids)
+        MusicDatabase.refreshMaterializedViews()
 
     def info(self, ids_or_paths, currentlyPlaying=False):
         songs = []
@@ -1194,8 +1198,32 @@ class Bard:
         songIDs = MusicBrainzDatabase.songsWithoutMBData()
         MusicBrainzDatabase.updateMusicBrainzIDs(songIDs)
 
+    def updateAlbums(self, regenerate=False, verbose=False):
+        songIDsAndPaths = MusicDatabase.songsWithReleaseID(
+            onlyWithoutAlbums=not regenerate)
+
+        lastAlbumPath = None
+        albumID = None
+        for songID, songPath in songIDsAndPaths:
+            aPath = albumPath(songPath)
+            if aPath != lastAlbumPath:
+                albumID = MusicDatabase.getAlbumID(aPath)
+                lastAlbumPath = aPath
+
+            MusicDatabase.setSongInAlbum(songID, albumID)
+        MusicDatabase.removeOrphanAlbums()
+        MusicDatabase.refreshMaterializedViews()
+
     def checkMusicBrainzTags(self, verbose=False):
-        MusicBrainzDatabase.checkMusicBrainzTags()
+        r1 = MusicBrainzDatabase.checkMusicBrainzTags()
+        r2 = MusicBrainzDatabase.checkAlbumsWithDifferentReleases()
+        r3 = MusicBrainzDatabase.checkAlbumsWithDifferentFormats()
+        if not any([r1, r2, r3]):
+            print('Musicbrainz tags are ok!')
+            print('This means that every song in the '
+                  'musicbrainzTaggedMusicPaths directories\n'
+                  'is tagged correctly and every directory only '
+                  'contains one release.')
 
     def cacheMusicBrainzDB(self, verbose=False):
         MusicBrainzDatabase.cacheMusicBrainzDB()
@@ -1540,6 +1568,15 @@ cache-musicbrainz-db [-v]
                                 'the database musicbrainz IDs from songs')
         parser.add_argument('-v', '--verbose', dest='verbose',
                             action='store_true', help='Be verbose')
+        # update-albums command
+        parser = sps.add_parser('update-albums', description='Update '
+                                'the albums to which songs belong to (this '
+                                'should be never needed)')
+        parser.add_argument('-r', '--regenerate', dest='regenerate',
+                            action='store_true', help='Reassign the album '
+                            'of all songs')
+        parser.add_argument('-v', '--verbose', dest='verbose',
+                            action='store_true', help='Be verbose')
         # check-musicbrainz-tags command
         parser = sps.add_parser('check-musicbrainz-tags', description='Check '
                                 'if there are songs which should have correct '
@@ -1650,6 +1687,9 @@ cache-musicbrainz-db [-v]
             self.backupMusic(options.target)
         elif options.command == 'update-musicbrainz-ids':
             self.updateMusicBrainzIDs(verbose=options.verbose)
+        elif options.command == 'update-albums':
+            self.updateAlbums(regenerate=options.regenerate,
+                              verbose=options.verbose)
         elif options.command == 'check-musicbrainz-tags':
             self.checkMusicBrainzTags(verbose=options.verbose)
         elif options.command == 'cache-musicbrainz-db':

bard/musicbrainz.sql
@@ -1582,8 +1582,8 @@ CREATE TABLE musicbrainz.l_work_work (
 );
 
 
--- This table doesn't come from MB. To fill it with data,
--- run "bard cache-mb-data"
+-- These tables don't come from MB. To fill artists_mb with data,
+-- run "bard cache-musicbrainz-db"
 CREATE TABLE artists_mb (
        id INTEGER PRIMARY KEY,
 
@@ -1598,3 +1598,61 @@ CREATE TABLE artists_mb (
           REFERENCES musicbrainz.artist(id)
 );
 
+CREATE TABLE albums (
+       id SERIAL PRIMARY KEY,
+       path TEXT UNIQUE
+);
+
+CREATE TABLE album_songs (
+       song_id INTEGER,
+       album_id INTEGER,
+
+       FOREIGN KEY(song_id)
+          REFERENCES songs(id),
+       FOREIGN KEY(album_id)
+          REFERENCES albums(id)
+);
+
+-- CREATE TABLE album_release (
+--        album_id INTEGER,
+--        release_id INTEGER,
+--
+--        FOREIGN KEY(album_id)
+--           REFERENCES albums(id),
+--        FOREIGN KEY(release_id)
+--           REFERENCES musicbrainz.release(id)
+-- );
+
+CREATE MATERIALIZED VIEW album_release
+                      AS select album_songs.album_id, musicbrainz.release.id release_id
+                           from songs_mb, album_songs, musicbrainz.release
+                          where releaseid = mbid
+                            and songs_mb.song_id = album_songs.song_id
+                       group by album_songs.album_id, musicbrainz.release.id;
+
+CREATE UNIQUE INDEX ON album_release (album_id);
+CREATE INDEX ON album_release (release_id);
+
+
+-- SELECT album_id, string_agg(distinct format,'+'), min(bitrate) min_bitrate, max(bitrate) max_bitrate from album_songs, properties p where album_songs.song_id = p.song_id group by album_songs.album_id
+
+CREATE MATERIALIZED VIEW album_properties
+                      AS SELECT album_id, format,
+                                min(bitrate) min_bitrate, max(bitrate) max_bitrate,
+                                min(bits_per_sample) min_bits_per_sample, max(bits_per_sample) max_bits_per_sample,
+                                min(sample_rate) min_sample_rate, max(sample_rate) max_sample_rate,
+                                min(channels) min_channels, max(channels) max_channels
+                           from album_songs, properties p
+                          where album_songs.song_id = p.song_id
+                       group by album_songs.album_id, format;
+--       format TEXT,
+--       min_bitrate INTEGER, -- 128, 320
+--       max_bitrate INTEGER,
+--       min_channels INTEGER, -- 2, 6
+--       max_channels INTEGER,
+--       min_bits_per_sample INTEGER, -- 16, 24
+--       max_bits_per_sample INTEGER,
+--       min_samplerate INTEGER,  -- 44100, 96000
+--       max_samplerate INTEGER,
+
+CREATE INDEX ON album_properties (album_id);

bard/musicbrainz_database.py
@@ -303,15 +303,66 @@ class MusicBrainzDatabase:
                    '                   FROM songs_mb '
                    '                  WHERE recordingid is not NULL)'
                    '     ORDER BY 1')
-        print('Songs which should have musicbrainz tags but don\'t:')
-        table = [('ID', 'PATH')]
+        table = []
         for root in config['musicbrainzTaggedMusicPaths']:
             result = c.execute(sql, {'root': root})
             table.extend((str(song_id), path)
                          for song_id, path in result.fetchall())
-        aligned = alignColumns(table, (False, True))
-        for line in aligned:
-            print(line)
+        if table:
+            table.insert(0, ('SONGID', 'PATH'))
+            aligned = alignColumns(table, (False, True))
+            print('Songs which should have musicbrainz tags but don\'t:')
+            for line in aligned:
+                print(line)
+        return bool(table)
+
+    @staticmethod
+    def checkAlbumsWithDifferentReleases():
+        c = MusicDatabase.getCursor()
+        sql = text('SELECT album_id, path, '
+                   '       COUNT(DISTINCT musicbrainz.release.id) '
+                   '  FROM songs_mb, albums, album_songs, musicbrainz.release '
+                   ' WHERE albums.id = album_songs.album_id '
+                   '   AND releaseid = mbid '
+                   '   AND songs_mb.song_id = album_songs.song_id '
+                   ' GROUP BY album_songs.album_id, albums.path '
+                   ' HAVING COUNT(DISTINCT musicbrainz.release.id) > 1')
+
+        result = c.execute(sql)
+        table = [(str(album_id), path, str(count))
+                 for album_id, path, count in result.fetchall()]
+        if table:
+            table.insert(0, ('ALBUMID', 'PATH', 'NUMBER OF RELEASES'))
+            aligned = alignColumns(table, (False, True, False))
+            print('Albums that contain songs from different releases:')
+            for line in aligned:
+                print(line)
+        return bool(table)
+
+    @staticmethod
+    def checkAlbumsWithDifferentFormats():
+        c = MusicDatabase.getCursor()
+        sql = text('select id, path, format '
+                   '  from albums, album_properties '
+                   ' where id in (select album_id '
+                   '                from (select  album_id, count(*) '
+                   '                        from album_properties '
+                   '                    group by album_id '
+                   '                      having count(*)>1) '
+                   '                  as foo) '
+                   '   and id = album_id')
+
+        result = c.execute(sql)
+        table = [(str(album_id), path, audioFormat)
+                 for album_id, path, audioFormat in result.fetchall()]
+        if table:
+            table.insert(0, ('ALBUMID', 'PATH', 'FORMAT'))
+            aligned = alignColumns(table, (False, True, True))
+            print('Albums that contain songs with different formats:')
+            for line in aligned:
+                print(line)
+        return bool(table)
+
 
     @staticmethod
     def get_all_artists():

bard/song.py
@@ -10,6 +10,7 @@ from bard.musicdatabase import MusicDatabase
 from bard.normalizetags import getTag
 from bard.ffprobemetadata import FFProbeMetadata
 from bard.terminalcolors import TerminalColors
+from bard.album import albumPath
 from pydub import AudioSegment
 from pydub.exceptions import PydubException
 from sqlalchemy import text
@@ -331,6 +332,9 @@ class Song:
             self.id = prevSong.id
             prevSong._path = self._path
             prevSong._root = self._root
+
+            albumID = MusicDatabase.getAlbumID(albumPath(self._path))
+            MusicDatabase.setSongInAlbum(self.id, albumID)
         return r
 
     def root(self):

commit b60949ec67ccae50d4d5d69860ba77a05d023f2d

Fix adding new songs which was broken by 54a41648acb1d5
All the code that adds information about a song to the database
has to use the same connection since otherwise foreign keys
don't work

bard/musicdatabase.py
@@ -773,7 +773,7 @@ CREATE TABLE similarities(
                     print(sql, tags)
                     raise
             albumID = MusicDatabase.getAlbumID(albumPath(song.path()))
-            MusicDatabase.setSongInAlbum(song.id, albumID)
+            MusicDatabase.setSongInAlbum(song.id, albumID, connection=c)
 
     @staticmethod
     def removeSong(song=None, byID=None):
@@ -1486,7 +1486,8 @@ or name {like} '%%MusicBrainz/Track Id'""")
         return MusicDatabase.engine.execute(executable)
 
     @staticmethod
-    def insert_or_update(table, recorddict, wherestatement=None):
+    def insert_or_update(table, recorddict, wherestatement=None,
+                         *, connection=None):
         if type(table) == str:
             table = MusicDatabase.table(table)
 
@@ -1494,7 +1495,9 @@ or name {like} '%%MusicBrainz/Track Id'""")
             wherestatement = (table.c.id == recorddict['id'])
 
         dbRecord = table.select(wherestatement)
-        dbRecord = MusicDatabase.execute(dbRecord).fetchone()
+        if not connection:
+            connection = MusicDatabase.getCursor()
+        dbRecord = connection.execute(dbRecord).fetchone()
         if dbRecord:
             if any(dbRecord[k] != recorddict[k]
                    for k in [c.name for c in table.columns]
@@ -1503,13 +1506,13 @@ or name {like} '%%MusicBrainz/Track Id'""")
                 u = table.update() \
                          .where(wherestatement) \
                          .values(**recorddict)
-                MusicDatabase.execute(u)
+                connection.execute(u)
 #            else:
 #                print(f'no changes in {table.name} db data for {recorddict}')
         else:
             print(f'insert {table.name} db data for {recorddict}')
             i = table.insert().values(**recorddict)
-            MusicDatabase.execute(i)
+            connection.execute(i)
 
     @staticmethod
     def songsWithReleaseID(onlyWithoutAlbums=True):
@@ -1552,11 +1555,12 @@ or name {like} '%%MusicBrainz/Track Id'""")
             return albumID
 
     @staticmethod
-    def setSongInAlbum(songID, albumID):
+    def setSongInAlbum(songID, albumID, *, connection=None):
         record = {'song_id': songID, 'album_id': albumID}
         album_songs = MusicDatabase.table('album_songs')
         MusicDatabase.insert_or_update(album_songs, record,
-                                       album_songs.c.song_id == songID)
+                                       album_songs.c.song_id == songID,
+                                       connection=connection)
 
     @staticmethod
     def removeOrphanAlbums():