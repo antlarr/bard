d939fd8ca75c26586f77a69c2c0e0a1d2e92ae1e
Antonio Larrosa <antonio.larrosa@gmail.com>
2020-01-26 20:10:48 +0100

    Bump version and fix project state

--- bard/__init__.py
+++ bard/__init__.py
@@ -6,9 +6,9 @@ from .bard import Bard
 __all__ = (Bard)
 __name__ = 'bard'
 __license__ = 'GPLv3'
-__version__ = '0.1'
-__date__ = '20180128'
+__version__ = '0.3.0'
+__date__ = '20200126'
 __author__ = 'Antonio Larrosa'
 __maintainer__ = 'Antonio Larrosa'
 __email__ = 'larrosa@kde.org'
-__status__ = 'Production'
+__status__ = 'Beta'
