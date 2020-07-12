f4304e7e57dc3828d8a48e8aad1e9152a0a6ff21
Antonio Larrosa <antonio.larrosa@gmail.com>
2020-07-12 21:12:18 +0200

    Bump version to 0.4.0

--- bard/__init__.py
+++ bard/__init__.py
@@ -2,8 +2,8 @@
 
 __name__ = 'bard'
 __license__ = 'GPLv3'
-__version__ = '0.3.2'
-__date__ = '20200427'
+__version__ = '0.4.0'
+__date__ = '20200712'
 __author__ = 'Antonio Larrosa'
 __maintainer__ = 'Antonio Larrosa'
 __email__ = 'larrosa@kde.org'
