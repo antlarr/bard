abc0e558b98dfd323245cddc508a3c433133009c
Antonio Larrosa <antonio.larrosa@gmail.com>
2020-07-19 11:19:00 +0200

    Add very simple parsing of arguments and cmd help to audiofile test script

--- bard/audiofile/pytest.py
+++ bard/audiofile/pytest.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 from bard import bard_audiofile
 import sys
+try:
+    input_file = sys.argv[1]
+except IndexError:
+    print('Error: please, specify an input file '
+          '(and optionally an output raw file)')
+
+try:
+    output_file = sys.argv[2]
+except IndexError:
+    output_file = 'output.raw'
 
 # x = bard_audiofile.decode(path='test4-header-missing.mp3')
 # with open('test4-header-missing.mp3', 'rb') as f:
@@ -8,9 +18,10 @@ import sys
 
 # x = bard_audiofile.decode(path='test4-header-missing.mp3')
 # x = bard_audiofile.decode(data=data)
-x = bard_audiofile.decode(path=sys.argv[1])
+x = bard_audiofile.decode(path=input_file)
 
-with open('output.raw', 'wb') as f:
+
+with open(output_file, 'wb') as f:
     f.write(x[0])
 
 print(x[1])
