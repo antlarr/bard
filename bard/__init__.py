dc3954b88b80d93a2ac46fbb14a67d6c0d7a42c3
Antonio Larrosa <antonio.larrosa@gmail.com>
2021-01-10 20:11:50 +0100

    Bump version to 0.5.0

--- bard/__init__.py
+++ bard/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Bard Music Manager."""
 
 __name__ = 'bard'
 __license__ = 'GPLv3'
-__version__ = '0.4.0'
-__date__ = '20200712'
+__version__ = '0.5.0'
+__date__ = '20210110'
 __author__ = 'Antonio Larrosa'
 __maintainer__ = 'Antonio Larrosa'
 __email__ = 'larrosa@kde.org'
